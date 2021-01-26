ba17c7bfd4b60818fe3a6520458e32861034307e
Jessica Paczuski <jessica@figment.network>
2021-01-26 18:40:04 +0000

    Index ibc txs

---
+++ api/mapper/ibc_channel.go
@@ -0,0 +1,23 @@
+package mapper
+
+import (
+	"errors"
+
+	shared "github.com/figment-networks/indexer-manager/structs"
+
+	channel "github.com/cosmos/cosmos-sdk/x/ibc/core/04-channel/types"
+	"github.com/gogo/protobuf/proto"
+)
+
+// IBCChannelOpenInitToSub transforms ibc.MsgSubmitMisbehaviour sdk messages to SubsetEvent
+func IBCChannelOpenInitToSub(msg []byte) (se shared.SubsetEvent, err error) {
+	m := &channel.MsgChannelOpenInit{}
+	if err := proto.Unmarshal(msg, m); err != nil {
+		return se, errors.New("Not a channel_open_init type" + err.Error())
+	}
+
+	return shared.SubsetEvent{
+		Type:   []string{"channel_open_init"},
+		Module: "ibc",
+	}, nil
+}
